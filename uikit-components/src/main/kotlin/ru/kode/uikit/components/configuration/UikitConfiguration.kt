1ac1f893f03784e4d69564e454bb240e4c15d598
Sergey <ssh@kode.ru>
2024-10-23 17:52:38 +0300

    [AT-256] Add radio button

--- uikit-components/src/main/kotlin/ru/kode/uikit/components/configuration/UikitConfiguration.kt
+++ uikit-components/src/main/kotlin/ru/kode/uikit/components/configuration/UikitConfiguration.kt
@@ -3,10 +3,12 @@ package ru.kode.uikit.components.configuration
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import ru.kode.uikit.components.CheckboxConfiguration
+import ru.kode.uikit.components.RadioButtonConfiguration
 
 @Immutable
 data class UikitConfiguration(
   val checkbox: CheckboxConfiguration,
+  val radioButton: RadioButtonConfiguration,
 )
 
 val LocalUikitConfiguration = staticCompositionLocalOf<UikitConfiguration> {
